8fedbac9ca3431434167e9dcf74e7751adf69dd0
yatiyr <dereeren@yahoo.com>
2022-08-15 15:34:22 +0300

    op premake has been updated.

--- Opium/premake5.lua
+++ Opium/premake5.lua
@@ -15,7 +15,7 @@ project "Opium"
         "src/**.h",
         "src/**.cpp",
         "src/**.hpp",
-	    "src/**.cpp",
+	"src/**.cpp",
         "external/stb_image/**.h",
         "external/stb_image/**.cpp",
 	"external/tinyexr/**.h",
@@ -25,7 +25,9 @@ project "Opium"
         "external/glm/glm/**.inl",
         "external/entt/include/**.hpp",
         "external/ImGuizmo/*.h",
-        "external/ImGuizmo/*.cpp"
+        "external/ImGuizmo/*.cpp",
+	"external/bullet/*.hpp",
+	"external/bullet/*.h"
     }
 
     defines
@@ -54,7 +56,8 @@ project "Opium"
         "%{IncludeDir.VK_SDK}",
 	"%{IncludeDir.FontAwesome}",
 	"%{IncludeDir.SPIRV_Cross}",
-	"%{IncludeDir.Assimp}"
+	"%{IncludeDir.Assimp}",
+	"%{IncludeDir.Bullet}"
     }
 
     links
@@ -66,7 +69,7 @@ project "Opium"
         "yaml-cpp",
         "opengl32.lib",
 	"%{Library.Mono}",
-	"%{Library.Assimp}"
+	"%{Library.Assimp}",
     }
 
     filter "files:external/ImGuizmo/**.cpp"
@@ -90,7 +93,19 @@ project "Opium"
         {
             "%{Library.ShaderC_DBG}",
 			"%{Library.SPIRV_CROSS_DBG}",
-			"%{Library.SPIRV_CROSS_GLSL_DBG}"
+			"%{Library.SPIRV_CROSS_GLSL_DBG}",
+	"%{LibraryDir.BulletDbg}/Bullet3Collision_Debug.lib",
+	"%{LibraryDir.BulletDbg}/Bullet3Common_Debug.lib",
+	"%{LibraryDir.BulletDbg}/Bullet3Dynamics_Debug.lib",
+	"%{LibraryDir.BulletDbg}/Bullet3Geometry_Debug.lib",
+	"%{LibraryDir.BulletDbg}/Bullet3OpenCL_clew_Debug.lib",
+	"%{LibraryDir.BulletDbg}/BulletCollision_Debug.lib",
+	"%{LibraryDir.BulletDbg}/BulletDynamics_Debug.lib",
+	"%{LibraryDir.BulletDbg}/BulletInverseDynamics_Debug.lib",
+	"%{LibraryDir.BulletDbg}/BulletInverseDynamicsUtils_Debug.lib",
+	"%{LibraryDir.BulletDbg}/BulletSoftBody_Debug.lib",
+	"%{LibraryDir.BulletDbg}/ConvexDecomposition_Debug.lib",
+	"%{LibraryDir.BulletDbg}/LinearMath_Debug.lib"
         }
 
     filter "configurations:Release"
@@ -102,7 +117,19 @@ project "Opium"
         {
             "%{Library.ShaderC_REL}",
 			"%{Library.SPIRV_CROSS_REL}",
-			"%{Library.SPIRV_CROSS_GLSL_REL}"
+			"%{Library.SPIRV_CROSS_GLSL_REL}",
+	"%{LibraryDir.BulletRel}/Bullet3Collision.lib",
+	"%{LibraryDir.BulletRel}/Bullet3Common.lib",
+	"%{LibraryDir.BulletRel}/Bullet3Dynamics.lib",
+	"%{LibraryDir.BulletRel}/Bullet3Geometry.lib",
+	"%{LibraryDir.BulletRel}/Bullet3OpenCL_clew.lib",
+	"%{LibraryDir.BulletRel}/BulletCollision.lib",
+	"%{LibraryDir.BulletRel}/BulletDynamics.lib",
+	"%{LibraryDir.BulletRel}/BulletInverseDynamics.lib",
+	"%{LibraryDir.BulletRel}/BulletInverseDynamicsUtils.lib",
+	"%{LibraryDir.BulletRel}/BulletSoftBody.lib",
+	"%{LibraryDir.BulletRel}/ConvexDecomposition.lib",
+	"%{LibraryDir.BulletRel}/LinearMath.lib"
         }        
 
     filter "configurations:Dist"
